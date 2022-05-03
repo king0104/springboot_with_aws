bd2eacff6a1101b11726549fb1e5128cf7a55f36
king0104 <4545abc@naver.com>
2022-05-03 18:51:17 +0900

    5장. 스프링시큐리티

--- src/main/java/com/yoon/book/springboot/web/IndexController.java
+++ src/main/java/com/yoon/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.yoon.book.springboot.web;
 
+import com.yoon.book.springboot.confg.auth.LoginUser;
+import com.yoon.book.springboot.confg.auth.dto.SessionUser;
 import com.yoon.book.springboot.service.posts.PostsService;
 import com.yoon.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * 페이지에 관련된 컨트롤러는 모두 여기에 작성
@@ -16,10 +20,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+    private  final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        // @LoginUser로 대체
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user!=null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
